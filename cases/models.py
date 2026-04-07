3ac2668d07709e45cf9a9263e50891c8b452681c
JackEscalon <jacek.escalon@gmail.com>
2026-04-07 21:48:30 +0200

    fix M2M relation: move tags to Case model

--- cases/models.py
+++ cases/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Case(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='cases')
+
+    def __str__(self):
+        return self.name
--- media_ingest/models.py
+++ media_ingest/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cases.models import Case
 
-# Create your models here.
+class MediaFile(models.Model):
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    case = models.ForeignKey(Case, on_delete=models.CASCADE, related_name='media_files')
+
+    def __str__(self):
+        return f"MediaFile {self.id}"
